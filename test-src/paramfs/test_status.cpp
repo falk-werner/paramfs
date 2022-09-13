1de200446790c7604fb78bf67814e2c77b7a90a7
Falk Werner <falk.werner@gmx.net>
2022-09-13 20:17:27 +0200

    added basic unit test infrastructure

---
+++ test-src/paramfs/test_status.cpp
@@ -0,0 +1,36 @@
+#include "paramfs/status.hpp"
+#include <gtest/gtest.h>
+
+using paramfs::to_string;
+using paramfs::status;
+
+namespace
+{
+
+struct param
+{
+    std::string expected;
+    status code;
+};
+
+class status_test_fixture: public ::testing::TestWithParam<param> { };
+
+}
+
+TEST_P(status_test_fixture, to_string)
+{
+    auto const & p = GetParam();
+    ASSERT_EQ(p.expected, paramfs::to_string(p.code));
+}
+
+INSTANTIATE_TEST_CASE_P(status_test, status_test_fixture, ::testing::Values(
+    param{"good", status::good},
+    param{"bad", status::bad},
+    param{"bad (not implemented)", status::bad_not_implemented},
+    param{"bad (unknown path)", status::bad_unknown_path}
+));
+
+TEST(status, to_string_unknown_code)
+{
+    ASSERT_STREQ("bad (unknown status code)", paramfs::to_string(static_cast<status>(-42)));
+}
